d2cfd165599f27adc3e80bae6001c141c6722a81
amrit <mr.amrrit7@gmail.com>
2021-04-10 18:02:09 +0530

    added images folder, product.js file, created screen folder and included various files in it, added rating and and product components in it, added reacr-bootstrap library, react-router-dom and react-router-bootstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import Product from './screens/Product';
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+    <Header />
+    <main className='py-3'>  
+      <Container>
+       <Route path='/' component={HomeScreen} exact />
+       <Route path='/product/:id' component={Product} />
+      </Container>
+    </main>
+    <Footer />
+    </Router>
   );
 }
 
