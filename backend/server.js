2888035652a58630e7d7b0151233cb472a8caeb3
amrit <mr.amrrit7@gmail.com>
2021-07-04 11:04:32 +0530

    modified server and PaymentScreen files, added new orderController and orderRoutes files.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -16,11 +17,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("App is running Now.......")
+    res.send("Welcome to DigiWholesale!")
 })
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 app.use(notFound);
